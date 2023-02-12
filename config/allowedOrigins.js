8d7fe2acf4a8a77efb437b9df8bb91a4e81df96a
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2023-02-12 18:31:35 -0300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://e-commerce-front-eight.vercel.app/",
+  "https://e-commerce-front-eight.vercel.app",
 ];
 
 module.exports = allowedOrigins;
